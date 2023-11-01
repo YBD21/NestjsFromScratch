9a96940fa3aa00327bda3a289db0d7551121422e
YBD21 <santosh.deuja2000@gmail.com>
2023-11-01 08:39:11 +0545

    added message.service

---
+++ project/src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessageRepository } from './messages.repository';
+
+export class MessageService {
+  private messagesRepo: MessageRepository;
+  constructor() {
+    // Service is creating its own dependencies
+    // DONT DO THIS ON REAL APPS
+    this.messagesRepo = new MessageRepository();
+  }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
