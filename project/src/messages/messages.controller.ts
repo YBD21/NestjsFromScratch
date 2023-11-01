c88e8b134580cb9e2e544300aa715cb93c024514
YBD21 <santosh.deuja2000@gmail.com>
2023-11-01 08:43:12 +0545

    added MessageService into Controller

--- project/src/messages/messages.controller.ts
+++ project/src/messages/messages.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessageService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessageService;
+
+  constructor() {
+    //USE DEPENDENCY INJECTION 
+    this.messagesService = new MessageService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    // console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    // console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
