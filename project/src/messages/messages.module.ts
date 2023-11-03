50212e1dcf428e8f1b6910c0fb26be29acde2813
YBD21 <santosh.deuja2000@gmail.com>
2023-11-03 18:15:07 +0545

    added Dependency Injection

--- project/src/messages/messages.module.ts
+++ project/src/messages/messages.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessageService } from './messages.service';
+import { MessageRepository } from './messages.repository';
 
+// Providers meaning things the can be used as dependency for other classes
 @Module({
-  controllers: [MessagesController]
+  controllers: [MessagesController],
+  providers: [MessageService, MessageRepository],
 })
 export class MessagesModule {}
--- project/src/messages/messages.service.ts
+++ project/src/messages/messages.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { MessageRepository } from './messages.repository';
 
+@Injectable()
 export class MessageService {
   constructor(public messagesRepo: MessageRepository) {}
 
